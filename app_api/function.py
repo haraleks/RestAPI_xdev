071a3b103c22b8e2f71d33f49acb1aeeb946e68d
Aleks Kharchenko <haraleks88@gmail.com>
2020-05-03 00:26:41 +0300

    Ready 100%

---
+++ app_api/function.py
@@ -0,0 +1,69 @@
+from flask import abort
+
+from app_api import models, db
+
+
+def add_role(role_name):
+    role_all = models.Role.query.all()
+    for role in role_all:
+        if role.role_name == role_name:
+            print('Error. The role is in the database')
+            return 'Error. The role is in the database'
+    id = role_all[-1].id + 1
+    create_role = models.Role(id=id, role_name=role_name)
+    db.session.add(create_role)
+    db.session.commit()
+    role_list = db.session.query(models.Role).filter(models.Role.id == id).all()
+    print('add new role')
+    return role_list
+
+
+def check_request_user_change(req):
+    if 'full_name' in req and 'phone' in req:
+        full_name_replace = req['full_name']
+        phone_replace = req['phone']
+    elif not 'full_name' in req or not 'phone' in req:
+        abort(500)
+    return full_name_replace, phone_replace
+
+
+def filter_id(key_id):
+    user = db.session.query(models.Users).filter(models.Users.id == key_id).all()
+    if not user:
+        abort(400)
+    if len(user) >= 2:
+        abort(401)
+    user = user[0]
+    return user
+
+
+def check_same_user(name, phone):
+    user_all = models.Users.query.all()
+    if user_all:
+        for user in user_all:
+            if user.full_name == name and user.phone == phone:
+                abort(404)
+
+
+def add_user(f_name, t_phone):
+    user_all = models.Users.query.all()
+    check_same_user(f_name, t_phone)
+    if not user_all:
+        id = 0
+    else:
+        id = user_all[-1].id + 1
+    create_user = models.Users(id=id, full_name=f_name, phone=t_phone)
+    db.session.add(create_user)
+    db.session.commit()
+    user_list = filter_id(id)
+    return user_list
+
+
+def user_replace(id, name_replace, phone_replace):
+    user = filter_id(id)
+    if name_replace:
+        user.full_name = name_replace
+    if phone_replace:
+        user.phone = phone_replace
+    db.session.add(user)
+    db.session.commit()
--- app_api/routes.py
+++ app_api/routes.py
@@ -1,70 +1,7 @@
-from flask import jsonify, make_response, request, abort
+from flask import jsonify, make_response, request
 
-from app_api import app, models, db
-
-
-def add_role(role_name):
-    role_all = models.Role.query.all()
-    for role in role_all:
-        if role.role_name == role_name:
-            print('Error. The role is in the database')
-            return 'Error. The role is in the database'
-    id = role_all[-1].id + 1
-    create_role = models.Role(id=id, role_name=role_name)
-    db.session.add(create_role)
-    db.session.commit()
-    role_list = db.session.query(models.Role).filter(models.Role.id == id).all()
-    print('add new role')
-    return role_list
-
-
-def check_request_user_chenge(request):
-    if len(request) == 2:
-        full_name_replace = request['full_name']
-        phone_replace = request['phone']
-    elif not 'full_name' in request:
-        full_name_replace = ''
-        phone_replace = request['phone']
-    elif not 'phone' in request:
-        full_name_replace = request['full_name']
-        phone_replace = ''
-    return full_name_replace, phone_replace
-
-
-def filter_id(key_id):
-    user = db.session.query(models.Users).filter(models.Users.id == key_id).all()
-    if len(user) >=2:
-        abort(401)
-    user = user[0]
-    return user
-
-
-def add_user(f_name, t_phone):
-    user_all = models.Users.query.all()
-    if user_all:
-        for user in user_all:
-            if user.full_name == f_name and user.phone == t_phone:
-                abort(500)
-    if not user_all:
-        id = 0
-    else:
-        id = user_all[-1].id + 1
-    create_user = models.Users(id=id, full_name=f_name, phone=t_phone)
-    db.session.add(create_user)
-    db.session.commit()
-    user_list = filter_id(id)
-    return user_list
-
-
-def user_replace(id, name_replace, phone_replace):
-    user = filter_id(id)
-    if name_replace:
-        user.full_name = name_replace
-    if phone_replace:
-        user.phone = phone_replace
-    db.session.add(user)
-    db.session.commit()
-
+from app_api import app
+from app_api.function import *
 
 if not models.Role.query.all():
     role_list = ['admin', 'guest', 'vip']
@@ -85,14 +22,15 @@ def create_user():
     try:
         full_name = request.json['full_name']
         phone = request.json['phone']
-        new_user = add_user(full_name, phone)
-        user_dict = dict(id=new_user.id,
-                         full_name=new_user.full_name,
-                         phone=new_user.phone,
-                         created_on=new_user.created_on,
-                         updated_on=new_user.updated_on)
+        if full_name and phone:
+            new_user = add_user(full_name, phone)
+            user_dict = dict(id=new_user.id,
+                             full_name=new_user.full_name, phone=new_user.phone,
+                             created_on=new_user.created_on, updated_on=new_user.updated_on)
 
-        return jsonify({'new_user': user_dict}), 201
+            return jsonify({'new_user': user_dict}), 201
+        else:
+            abort(500)
     except KeyError:
         abort(500)
 
@@ -100,17 +38,17 @@ def create_user():
 @app.route('/api/edit_user/<int:id>',  methods=['PUT'])
 def edit_user(id):
     user = filter_id(id)
-    if not request.json or not 'full_name' in request.json and not 'phone' in request.json:
+    if not request.json:
         abort(400)
-    full_name_replace, phone_replace = check_request_user_chenge(request.json)
+    full_name_replace, phone_replace = check_request_user_change(request.json)
+    check_same_user(full_name_replace, phone_replace)
     if full_name_replace or phone_replace:
         user_replace(id, full_name_replace, phone_replace)
         user_dict = dict(id=user.id,full_name=user.full_name, phone=user.phone,
-                         created_on=user.created_on, updated_on=user.updated_on, role = user.role.role_name)
+                         created_on=user.created_on, updated_on=user.updated_on, role=user.role)
         return jsonify({'data changed': user_dict}), 201
     else:
         abort(400)
-
 
 
 @app.route('/api/give_role/<int:id>',  methods=['PUT'])
@@ -172,7 +110,7 @@ def get_users():
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': 'Data is in the database'}), 404)
 
 @app.errorhandler(400)
 def not_found(error):
